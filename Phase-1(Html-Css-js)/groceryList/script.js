ab554b56ff48c1220238b21ca5dbbf4779629b1c
reza <reza.nikravesh80@gmail.com>
2021-08-14 13:40:09 +0430

    handle remove button

--- Phase-1(Html-Css-js)/groceryList/script.js
+++ Phase-1(Html-Css-js)/groceryList/script.js
@@ -1,21 +1,57 @@
-///////
 let ul = document.querySelector("ul");
 let logo = document.querySelector(".container .img");
+
+function removeItem(name) {
+  let listItems = JSON.parse(localStorage.getItem("listItems"));
+
+  for (let index = 0; index < listItems.length; index++) {
+    if (name == listItems[index].name) listItems.splice(index, 1);
+  }
+  localStorage.setItem("listItems", JSON.stringify(listItems));
+  loadItems();
+}
+function editItem(name) {}
 function addItem(name) {
   logo.style.display = "none";
-  let item =
-    "<li><h3>" +
-    name +
-    '</h3><div class="buttons"><i class="bx bxs-edit" style="color: #72ef07"></i><i class="bx bxs-trash-alt" style="color: #ff0000"></i></div></li>';
-  ul.innerHTML += item;
+
+  //crate elements
+  let li = document.createElement("li");
+  let h3 = document.createElement("h3");
+  let div = document.createElement("div");
+  let ic_remove = document.createElement("i");
+  let ic_edit = document.createElement("i");
+
+  //add atributes
+  h3.innerHTML = name;
+  div.className = "buttons";
+  ic_edit.className = "bx bxs-edit";
+  ic_edit.style.color = "#72ef07";
+  ic_remove.className = "bx bxs-trash-alt";
+  ic_remove.style.color = "#ff0000";
+
+  //apend
+  div.appendChild(ic_remove);
+  div.appendChild(ic_edit);
+  li.appendChild(h3);
+  li.appendChild(div);
+  ul.appendChild(li);
+
+  // add listener
+  ic_edit.addEventListener("click", () => {
+    editItem(name);
+  });
+  ic_remove.addEventListener("click", () => {
+    removeItem(name);
+  });
 }
 function loadItems() {
+  ul.innerHTML = "";
+
   if (
     localStorage.getItem("listItem") === null ||
     JSON.parse(localStorage.getItem("listItem")).length === 0
   )
     logo.style.display = "block";
-
   for (
     let item = 0;
     item < JSON.parse(localStorage.getItem("listItems")).length;
@@ -24,7 +60,6 @@ function loadItems() {
     addItem(JSON.parse(localStorage.getItem("listItems"))[item].name);
   }
 }
-
 function submitHandler(e) {
   let input = document.querySelector(".form input");
   let form = document.querySelector(".form form");
