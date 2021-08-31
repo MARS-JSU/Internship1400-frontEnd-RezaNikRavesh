0ec83c6f642b184e658a7765fc9bb2789dbcf72a
reza <reza.nikravesh80@gmail.com>
2021-08-31 19:47:56 +0430

    create footer

---
+++ Wallpaper Land/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        Copyright © 2021 Reza Nikravesh - All Rights Reserved
+      </p>
+    </div>
+  );
+}
+export default Footer;
--- Wallpaper Land/src/components/layout/Layout.js
+++ Wallpaper Land/src/components/layout/Layout.js
@@ -1,10 +1,12 @@
 import MainNavigation from "./MainNavigation";
 import styles from "./Layout.module.css";
+import Footer from "./Footer";
 function Layout({ children }) {
   return (
     <main className={styles.container}>
       <MainNavigation />
       {children}
+      <Footer />
     </main>
   );
 }
